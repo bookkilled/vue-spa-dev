b091212c9b263c7e72ec6b5e07dfca071009ecb8
bookkilled <bookkilled@gmail.com>
2017-07-06 22:26:11 +0800

    dh

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+    <transition name="fadeOutLeft">
      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -16,5 +18,21 @@ export default {
 </script>
 
 <style>
-
+.outLeftInRight-transition,
+.fadeOutLeft-transition, 
+.fadeInLeft-transition {
+  transition: transform .3s ease-in-out;
+}
+/* 进入时的状态，从右开始 */
+.outLeftInRight-enter,
+.fadeInLeft-enter,
+.fadeInLeft-leave {
+  transform: translateX(100%);
+}
+/* 离开时的状态，向左移动 */
+.outLeftInRight-leave,
+.fadeOutLeft-enter,
+.fadeOutLeft-leave {
+  transform: translateX(-100%);
+}
 </style>
